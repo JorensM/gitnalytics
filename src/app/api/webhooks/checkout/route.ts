cb96f515a2548805d4443fd4a86b9c1fbe3a5c0b
JorensM <jorensmerenjanu@gmail.com>
2025-03-27 15:52:35 +0200

    docs: add comment explaining the purpose of the checkout webhook

--- src/app/api/webhooks/checkout/route.ts
+++ src/app/api/webhooks/checkout/route.ts
@@ -1,5 +1,9 @@
 import { NextResponse } from 'next/server';
 
+// This webhook is called upon successful checkout of a 
+// new user. It should create an account for the new user
+// with the credentials based on the provided metadata in the request
+// body
 export async function POST() {
     console.log('webhook called');
 
