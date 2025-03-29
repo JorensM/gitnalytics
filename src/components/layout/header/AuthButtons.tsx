b6a5c2a1496853b905cb293362d4e780afc78642
JorensM <jorensmerenjanu@gmail.com>
2025-03-29 19:56:56 +0200

    feat: show login button on register page and register button on login page

--- src/components/layout/header/AuthButtons.tsx
+++ src/components/layout/header/AuthButtons.tsx
@@ -7,15 +7,24 @@ export default function AuthButtons() {
 
     const pathname = usePathname();
 
-    if(pathname === '/') {
+    const showLoginButton = ['/', '/register'].includes(pathname);
+    const showRegisterButton = ['/', '/login'].includes(pathname);
+
+    const showAnything = showLoginButton || showRegisterButton;
+
+    if(showAnything) {
         return (
             <div className='w-fit flex gap-8 items-center'>
-                <Link href='/login'>
-                    Sign In
-                </Link>
-                <Link href='/register' className='border border-blue-800 p-2 text-blue-500 hover:bg-blue-800 hover:text-blue-200 rounded-sm'>
-                    Register
-                </Link>
+                {showLoginButton ? 
+                    <Link href='/login'>
+                        Sign In
+                    </Link>
+                : null}
+                {showRegisterButton ? 
+                    <Link href='/register' className='border border-blue-800 p-2 text-blue-500 hover:bg-blue-800 hover:text-blue-200 rounded-sm'>
+                        Register
+                    </Link>
+                : null}
             </div>
         )
     } else {
