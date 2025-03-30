a3c99538098de70927441995202b62422398bbdd
JorensM <jorensmerenjanu@gmail.com>
2025-03-30 17:12:42 +0300

    fix: change 'reset password' to 'change password'

--- src/app/dashboard/settings/page.tsx
+++ src/app/dashboard/settings/page.tsx
@@ -5,7 +5,7 @@ export default function SettingsPage() {
     return (
         <div className='flex flex-col gap-4'>
             <ManageSubscriptionButton/>
-            <Link href='/register'>Reset Password</Link>
+            <Link href='/register'>Change Password</Link>
         </div>
     )
 }
