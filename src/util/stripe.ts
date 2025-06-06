8045d9778661651858ca28dc57448bd2da7b766b
Jorens Merenjanu <jorensmerenjanu@gmail.com>
2025-06-06 17:28:07 +0300

    Update stripe.ts

--- src/util/stripe.ts
+++ src/util/stripe.ts
@@ -3,63 +3,65 @@ import createStripeClient from './createStripeClient';
 import { createClient } from './supabase/server';
 
 export const getStripeCustomerID = async () => {
-    const supabase = await createClient();
-    const { data: { user }, error } = await supabase.auth.getUser();
-    if(error){
-        throw 'Supabase error: ' + error;
-    }
-    if(!user) {
-        throw 'No user found';
-    }
-    const stripeCustomerID = user?.user_metadata.stripe_customer_id as string;
-    return stripeCustomerID;
+    // const supabase = await createClient();
+    // const { data: { user }, error } = await supabase.auth.getUser();
+    // if(error){
+    //     throw 'Supabase error: ' + error;
+    // }
+    // if(!user) {
+    //     throw 'No user found';
+    // }
+    // const stripeCustomerID = user?.user_metadata.stripe_customer_id as string;
+    // return stripeCustomerID;
 }
 
 export const getSubscriptionActive = async () => {
-    const stripe = createStripeClient();
+    return true;
+    // const stripe = createStripeClient();
 
-    const stripeCustomerID = await getStripeCustomerID();
+    // const stripeCustomerID = await getStripeCustomerID();
 
-    const { data: subscriptions } = await stripe.subscriptions.list({
-        customer: stripeCustomerID
-    })
+    // const { data: subscriptions } = await stripe.subscriptions.list({
+    //     customer: stripeCustomerID
+    // })
 
-    if(subscriptions.length === 0) {
-        return false;
-    } else {
-        return subscriptions.some(subscription => !subscription.ended_at)
-    }
+    // if(subscriptions.length === 0) {
+    //     return false;
+    // } else {
+    //     return subscriptions.some(subscription => !subscription.ended_at)
+    // }
 }
 
 export const getSubscriptionCancelled = async () => {
-    const stripe = createStripeClient();
+    return false;
+    // const stripe = createStripeClient();
 
-    const stripeCustomerID = await getStripeCustomerID();
+    // const stripeCustomerID = await getStripeCustomerID();
 
-    const { data: subscriptions } = await stripe.subscriptions.list({
-        customer: stripeCustomerID
-    })
+    // const { data: subscriptions } = await stripe.subscriptions.list({
+    //     customer: stripeCustomerID
+    // })
 
-    if(subscriptions.length === 0) {
-        return true;
-    } else {
-        return subscriptions.every(subscription => subscription.canceled_at)
-    }
+    // if(subscriptions.length === 0) {
+    //     return true;
+    // } else {
+    //     return subscriptions.every(subscription => subscription.canceled_at)
+    // }
 }
 
 export async function getSubscriptionStatus() {
-    const stripe = createStripeClient();
+    // const stripe = createStripeClient();
 
-    const stripeCustomerID = await getStripeCustomerID();
+    // const stripeCustomerID = await getStripeCustomerID();
 
     const isActive = await getSubscriptionActive();
     const isCancelled = await getSubscriptionCancelled();
     const trial = null;
     let daysLeft = 0;
 
-    const { data: subscriptions } = await stripe.subscriptions.list({
-        customer: stripeCustomerID
-    })
+    // const { data: subscriptions } = await stripe.subscriptions.list({
+    //     customer: stripeCustomerID
+    // })
 
     // console.log(subscriptions);
 
@@ -85,24 +87,25 @@ export async function getSubscriptionStatus() {
 
 // todo: Refactor to not be async and not depend on stripe functions, rather just accept a subscriptionStatus arg
 export async function getSubscriptionStatusMessage(_isActive?: boolean) {
-    const stripe = createStripeClient();
+    return "message";
+    // const stripe = createStripeClient();
 
-    const stripeCustomerID = await getStripeCustomerID();
+    // const stripeCustomerID = await getStripeCustomerID();
 
-    const isActive = typeof _isActive === 'undefined' ? !(await getSubscriptionCancelled()) : _isActive;
+    // const isActive = typeof _isActive === 'undefined' ? !(await getSubscriptionCancelled()) : _isActive;
 
-    const subscriptionStatus = await getSubscriptionStatus();
+    // const subscriptionStatus = await getSubscriptionStatus();
 
-    if(isActive) {
-        return 'Subscription active';
-    } else if(subscriptionStatus.ended) {
-        return 'Subscription ended';
-    } else {
-        const { data: subscriptions } = await stripe.subscriptions.list({
-            customer: stripeCustomerID
-        })
-        const subscription = subscriptions[0];
-        const daysLeft = moment.unix(subscription.cancel_at!).diff(moment(), 'days');
-        return daysLeft + ' days until subscription ends';
-    }
+    // if(isActive) {
+    //     return 'Subscription active';
+    // } else if(subscriptionStatus.ended) {
+    //     return 'Subscription ended';
+    // } else {
+    //     const { data: subscriptions } = await stripe.subscriptions.list({
+    //         customer: stripeCustomerID
+    //     })
+    //     const subscription = subscriptions[0];
+    //     const daysLeft = moment.unix(subscription.cancel_at!).diff(moment(), 'days');
+    //     return daysLeft + ' days until subscription ends';
+    // }
 }
