59aa88b7bace2d444e139884e16b257d69c6a0bb
Jorens Merenjanu <jorensmerenjanu@gmail.com>
2025-06-06 17:29:27 +0300

    Update createStripeClient.ts

--- src/util/createStripeClient.ts
+++ src/util/createStripeClient.ts
@@ -1,10 +1,10 @@
-import Stripe from 'stripe';
+// import Stripe from 'stripe';
 
 export default function createStripeClient() {
-    console.log(process.env.STRIPE_SECRET_KEY);
-    if(!process.env.STRIPE_SECRET_KEY) {
-        console.log('throwing');
-        throw new Error('STRIPE_SECRET_KEY env var not set')
-    }
-    return new Stripe(process.env.STRIPE_SECRET_KEY!);
+    // console.log(process.env.STRIPE_SECRET_KEY);
+    // if(!process.env.STRIPE_SECRET_KEY) {
+    //     console.log('throwing');
+    //     throw new Error('STRIPE_SECRET_KEY env var not set')
+    // }
+    return null;//new Stripe(process.env.STRIPE_SECRET_KEY!);
 }
