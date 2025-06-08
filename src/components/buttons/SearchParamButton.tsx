c7f78a02940fc8a1b60c60ba3dd09c167610791e
JorensM <jorensmerenjanu@gmail.com>
2025-06-08 18:39:51 +0300

    fix: missing import error

--- src/components/buttons/SearchParamButton.tsx
+++ src/components/buttons/SearchParamButton.tsx
@@ -4,13 +4,15 @@ type SearchParamButtonProps = {
     default: string
 }
 
+// WIP
+// A button that changes its label based on current URL search params
 export default function SearchParamButton(props: SearchParamButtonProps) {
 
-    const params = useSearchParams();
+    // const params = useSearchParams();
 
-    const paramKey = Object.keys(props.params)[0];
+    // const paramKey = Object.keys(props.params)[0];
 
-    const label = params.get(paramKey) ? props.params[paramKey] : props.default;
+    // const label = params.get(paramKey) ? props.params[paramKey] : props.default;
 
     return <button>{label}</button>;
 }
