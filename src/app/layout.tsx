7b3b20ae0f44d80de4c56fff63ce02a01165d77b
JorensM <jorensmerenjanu@gmail.com>
2025-06-08 19:40:35 +0300

    fix: enable gtag and google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,18 +57,7 @@ export default async function RootLayout({
               {`window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               
-              gtag('consent', 'default', {
-                'ad_storage': 'denied',
-                'ad_user_data': 'denied',
-                'ad_personalization': 'denied',
-                'analytics_storage': 'denied'
-              });
-
-              
               gtag('js', new Date());
-                
-              gtag('config', 'G-GXM4YEPGPR', { 'storage': 'none' });
-
               `}
             </Script>
           </>
