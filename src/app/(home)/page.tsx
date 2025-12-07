4c0034c4b60be4fd824b94b0b7e4c051d34e4864
Jorens Merenjanu <jorensmerenjanu@gmail.com>
2025-12-07 08:13:16 +0200

    Move feature list for Gitnalytics impact measurement higher up
    So that it can be seen in Google search results. Also style it.

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -15,6 +15,11 @@ export default async function Home() {
     <div className='flex flex-col p-8 max-w-[1000px] gap-4'>
       <h2 className='text-2xl'>Measure the impact of your product updates</h2>
       <img src='gitnalytics_2.PNG' className='w-full'/>
+      <ul className='my-2 list-disc'>
+        <li>See how your code changes impact your visitors.</li>
+        <li>Correlate Git commits with visitor traffic to measure real impact.</li>
+        <li>Link your GitHub commits with Google Analytics to understand which changes matter.</li>
+      </ul>
       <p>
         Gitnalytics allows you to map/compare your Git commit history to your user analytics
         to help you better understand which updates to your code leave the most
@@ -25,11 +30,7 @@ export default async function Home() {
         platforms will be integrated eventually. If you have a feature request,
         feel free to reach out at <MailLink/>
       </p>
-      <ul>
-        <li>See how your code changes impact your visitors.</li>
-        <li>Correlate Git commits with visitor traffic to measure real impact.</li>
-        <li>Link your GitHub commits with Google Analytics to understand which changes matter.</li>
-      </ul>
+      
     </div>
   );
 }
